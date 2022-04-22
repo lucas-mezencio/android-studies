3e11d39856cf423cc7ee9736cbc5306df5972ea5
Lucas Mezencio <lucasmezss@hotmail.com>
2022-04-22 16:33:33 -0300

    find views efficiently

--- app/src/main/java/com/codelab/mezencio/diceroller/MainActivity.kt
+++ app/src/main/java/com/codelab/mezencio/diceroller/MainActivity.kt
@@ -7,17 +7,21 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.iv_dice_image)
 
         val rollButton: Button = findViewById(R.id.btn_roll)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val drawableResource = when (randomInt) {
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -25,8 +29,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        val diceImage: ImageView = findViewById(R.id.iv_dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
