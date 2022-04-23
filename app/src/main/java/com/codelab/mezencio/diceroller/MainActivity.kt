85747945894672615f279e3bd4495184f0125a6f
Lucas Mezencio <lucasmezss@hotmail.com>
2022-04-23 13:58:41 -0300

    api levels and compatibility

--- app/src/main/java/com/codelab/mezencio/diceroller/MainActivity.kt
+++ app/src/main/java/com/codelab/mezencio/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.codelab.mezencio.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
