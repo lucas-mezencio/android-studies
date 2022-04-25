d499ee8f7dc76720b0adb53d3535d0f40a3b5a28
Lucas Mezencio <lucasmezss@hotmail.com>
2022-04-25 19:51:24 -0300

    add click listener to done button

--- AboutMe/app/src/main/java/com/codelabs/mezencio/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/codelabs/mezencio/aboutme/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.codelabs.mezencio.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextNickname: EditText
+    private lateinit var nicknameTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextNickname = findViewById(R.id.et_nickname)
+        nicknameTextView = findViewById(R.id.tv_nickname)
+
+        val doneBtn: Button = findViewById(R.id.btn_done)
+        doneBtn.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nicknameTextView.text = editTextNickname.text
+        nicknameTextView.visibility = View.VISIBLE
+        editTextNickname.visibility = View.GONE
+        view.visibility = View.GONE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
