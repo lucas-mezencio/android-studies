e73298c9fa656b75a64ec4d177994767314d8d0d
Lucas Mezencio <lucasmezss@hotmail.com>
2022-04-26 22:14:17 -0300

    add click listeners to buttons

--- ColorMyViews/app/src/main/java/com/codelabs/mezencio/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/codelabs/mezencio/colormyviews/MainActivity.kt
@@ -4,12 +4,21 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boxThree: TextView
+    private lateinit var boxFour: TextView
+    private lateinit var boxFive: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boxThree = findViewById(R.id.tv_box_three)
+        boxFour = findViewById(R.id.tv_box_four)
+        boxFive = findViewById(R.id.tv_box_five)
 
         setListeners()
     }
@@ -21,6 +30,11 @@ class MainActivity : AppCompatActivity() {
             R.id.tv_box_three -> view.setBackgroundColor(Color.BLUE)
             R.id.tv_box_four -> view.setBackgroundColor(Color.MAGENTA)
             R.id.tv_box_five -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.red_button -> boxThree.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFour.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFive.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -34,9 +48,15 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout)
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
 
         for (item in clickableViews) {
             item.setOnClickListener(this::makeColored)
