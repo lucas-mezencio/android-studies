d4560bf25d11b2b1bfe7ac5f01a8a3d9b2dbb91a
Lucas Mezencio <lucasmezss@hotmail.com>
2022-05-03 13:18:22 -0300

    add play again button - control navigation by livedata observers

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -21,6 +21,18 @@ class ScoreViewModel(finalScore: Int) : ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
+
     init {
         _score.value = finalScore
         Log.i("ScoreViewModel", "Final score is $finalScore")
