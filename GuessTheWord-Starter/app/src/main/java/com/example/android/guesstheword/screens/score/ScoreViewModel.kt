29302c26664fab994c64b382c7ac60cbcbceb03f
Lucas Mezencio <lucasmezss@hotmail.com>
2022-05-03 13:04:13 -0300

    add livedata to score viewmodel

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
@@ -15,8 +17,12 @@ class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Fac
 }
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
-    var score = finalScore
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
     init {
+        _score.value = finalScore
         Log.i("ScoreViewModel", "Final score is $finalScore")
     }
 }
